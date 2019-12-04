47d3447f799c04a77b1a7cb3cc8c76f8ce4336d5
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2019-12-04 20:13:19 +0200

    Create feature extractor

--- stransfer.py
+++ stransfer.py
@@ -32,3 +32,33 @@ style_layers = ['block1_conv1',
                 'block3_conv1', 
                 'block4_conv1', 
                 'block5_conv1']
+
+
+def create_feature_extractor(layer_names):
+  
+  vgg = tf.keras.applications.VGG16(include_top=False, weights='imagenet')
+  vgg.trainable = False
+
+  print(vgg.summary())
+
+  outputs = [vgg.get_layer(layer_name).output for layer_name in layer_names]
+
+  model = tf.keras.Model(vgg.inputs, outputs)
+
+  return model
+
+
+layers_of_interest = content_layers + style_layers
+feature_extractor = create_feature_extractor(layers_of_interest)
+content_prep = tf.keras.applications.vgg16.preprocess_input(content_img)
+content_prep = tf.image.resize(content_prep, size=(224, 224))
+content_prep = content_prep[tf.newaxis, :]
+outputs = feature_extractor(content_prep)
+
+for name, output in zip(layers_of_interest, outputs):
+  print(name)
+  print("  shape: ", output.numpy().shape)
+  print("  min: ", output.numpy().min())
+  print("  max: ", output.numpy().max())
+  print("  mean: ", output.numpy().mean())
+  print()
