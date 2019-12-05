003df7463b6ae47ff8ba6cd15ba523bc84c0710c
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2019-12-05 15:15:31 +0200

    Extract content and style features from the input images once before training

--- stransfer.py
+++ stransfer.py
@@ -9,7 +9,7 @@ print("TensorFlow Hub version:", hub.__version__)
 
 
 content_img = imageio.imread('data/content/chicago.jpg')
-
+style_img = imageio.imread('data/style/wave.jpg')
 
  # Content layers to get content feature maps
 content_layers = ['block4_conv2'] 
@@ -28,6 +28,7 @@ def preprocess_image(image):
   image_prep = image_prep[tf.newaxis, :]
   return image_prep
 
+
 def create_feature_extractor(layer_names):
   
   vgg = tf.keras.applications.VGG16(include_top=False, weights='imagenet')
@@ -44,10 +45,7 @@ def create_feature_extractor(layer_names):
 
 layers_of_interest = content_layers + style_layers
 feature_extractor = create_feature_extractor(layers_of_interest)
-#content_prep = tf.keras.applications.vgg16.preprocess_input(content_img)
-#content_prep = tf.image.resize(content_prep, size=(224, 224))
-#content_prep = content_prep[tf.newaxis, :]
-content_prep = preprocess_image(content_img)
+""" content_prep = preprocess_image(content_img)
 content_prep = tf.constant(content_prep)
 outputs = feature_extractor(content_prep)
 
@@ -58,17 +56,27 @@ for name, output in zip(layers_of_interest, outputs):
   print("  max: ", output.numpy().max())
   print("  mean: ", output.numpy().mean())
   print()
+ """
 
+
+# preprocess images
+content_prep = preprocess_image(content_img)
+style_prep = preprocess_image(style_img)
 
 # get content and style features only once before training
-content_features = get_features(content_prep, feature_extractor)
-style_features = get_features(style_prep, feature_extractor)
+content_features = feature_extractor(content_prep)
+content_features = content_features[:len(content_layers)]
+
+style_features = feature_extractor(style_prep)
+style_features = style_features[len(content_layers):]
+#content_features = get_features(content_prep, feature_extractor)
+#style_features = get_features(style_prep, feature_extractor)
 
 # calculate the gram matrices for each layer of our style representation
-style_grams = {layer: gram_matrix(style_features[layer]) for layer in style_features}
+#style_grams = {layer: gram_matrix(style_features[layer]) for layer in style_features}
 
 # create a third "target" image and prep it for change
 # it is a good idea to start off with the target as a copy of our *content* image
 # then iteratively change its style
-target = tf.Variable(content_prep)
+#target = tf.Variable(content_prep)
 #target = content.clone().requires_grad_(True).to(device)
