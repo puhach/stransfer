58737cf7e15501c762ce43e796d3e149c4c317c6
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2020-02-04 15:16:05 +0200

    Remove unnecessary input (tensorflow.keras.backend)

--- stransfer.py
+++ stransfer.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 #import tensorflow_hub as hub
-import tensorflow.keras.backend
+#import tensorflow.keras.backend
 import imageio
 #from featureextractor import *
 import numpy as np
